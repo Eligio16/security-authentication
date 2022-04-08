80962ddd8eca8d769cdc33cdab89c04e98dc6765
Eligio <eligio.eq@gmail.com>
2022-04-08 15:00:35 -0500

    level 1 user and pass

--- app.js
+++ app.js
@@ -1,23 +1,75 @@
+//Require dependencies
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
+//Definiendo app express
 const app = express();
 
+//Definiendo archivos locales, EJS, bodyparser
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+//Conexion local con la BD
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/userDB');
+}
+
+//Definiendo esquema
+const userSchema = {
+    email: String,
+    password: String
+};
+
+//Definiendo Modelo
+const User = mongoose.model('User', userSchema);
+
+//Metodos GET - Direccionamiento
 app.get('/', (req, res) =>{
     res.render('home');
 })
 
-app.get('/login', (req, res) =>{
+app.get('/login', (req, res) => {
     res.render('login');
 })
 
-app.get('/register', (req, res) =>{
+app.get('/register', (req, res) => {
     res.render('register');
+})
+
+//Metodos POST
+app.post('/register', (req,res) => {
+    const newUser = new User ({
+        email: req.body.username,
+        password: req.body.password
+    })
+    
+    newUser.save((err) => {
+        if (err){console.log(err);}
+        else{res.render('secrets')}
+    }
+    )
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser){
+                if (foundUser.password === password){
+                    res.render('secrets');
+                }
+            }
+        }
+    })
 })
 
 app.listen(3000, ()=>{
