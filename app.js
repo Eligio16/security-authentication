cb19e4c7f3867d8f81be6f03f61a8c1b0fc9e6c3
Eligio <eligio.eq@gmail.com>
2022-04-12 08:45:55 -0500

    Level 4 - salting and hashing passwords

--- app.js
+++ app.js
@@ -1,82 +1,87 @@
 //Require dependencies
-require('dotenv').config();
-const express = require('express');
-const ejs = require('ejs');
-const mongoose = require('mongoose');
-const md5 = require('md5');
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 //Definiendo app express
 const app = express();
 
-console.log(process.env.API_KEY);
-
 //Definiendo archivos locales, EJS, bodyparser
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //Conexion local con la BD
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/userDB');
+  await mongoose.connect("mongodb://localhost:27017/userDB");
 }
 
 //Definiendo esquema
 const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
+  email: String,
+  password: String,
 });
-
 
 //Definiendo Modelo
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 //Metodos GET - Direccionamiento
-app.get('/', (req, res) =>{
-    res.render('home');
-})
-
-app.get('/login', (req, res) => {
-    res.render('login');
-})
-
-app.get('/register', (req, res) => {
-    res.render('register');
-})
-
-//Metodos POST
-app.post('/register', (req,res) => {
-    const newUser = new User ({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
-    
-    newUser.save((err) => {
-        if (err){console.log(err);}
-        else{res.render('secrets')}
-    }
-    )
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.post('/login', (req, res) => {
-    const username = req.body.username;
-    const password = md5(req.body.password);
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-    User.findOne({email: username}, (err, foundUser) => {
-        if (err) {
-            console.log(err);
-        } else {
-            if (foundUser){
-                if (foundUser.password === password){
-                    res.render('secrets');
-                }
-            }
-        }
-    })
-})
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
-app.listen(3000, ()=>{
-    console.log("Running on port 3k");
-})
+//Metodos POST
+app.post("/register", (req, res) => {
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
+      }
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Running on port 3k");
+});
